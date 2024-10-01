63a2f22c7c39eca4a524df091d114708da9b42ef
Alexey <a504512@mail.ru>
2024-10-01 16:44:50 +0300

    добавил новые поля на страницу пользователя, сделал миграции, новый файл profile.css

--- login/models.py
+++ login/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFit
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Review(models.Model):
   originalfoto=models.ImageField(upload_to='image', verbose_name="своё фото")
@@ -30,3 +32,18 @@ class Portfolio(models.Model):
    
   def __str__(self) -> str:
     return "работа " + str(self.id)
+  
+class Profile(models.Model):
+  originalfoto=models.ImageField(upload_to='image', verbose_name="своё фото")
+  foto=ImageSpecField(source='originalfoto', format='PNG', processors=[ResizeToFit(200,200)])
+  date=models.DateField(verbose_name="дата рождения", auto_now=False, auto_now_add=False, null=True)
+  firstname=models.CharField(max_length=20, verbose_name="имя")
+  lastname=models.CharField(max_length=20, verbose_name="фамилия")
+  user=models.OneToOneField(User, related_name="Profile", on_delete=models.CASCADE, null=True)
+@receiver(post_save, sender=User)
+def createProfile(sender, instance, created, **kwargs):
+  if created:
+    Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def saveProfile(sender, instance, **kwargs):
+  instance.Profile.save()
